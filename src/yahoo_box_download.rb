51216b39e862246e7d66f08d1b03c21a213cafe3
miyawa-tarou <webmaster@dojin-music.info>
2018-10-07 11:08:11 +0900

    1フォルダから100ファイル以上取得するなどの修正
    - 1フォルダに100ファイル以上ある場合も取得できるように
    - 既にファイルやディレクトリがある場合はスキップ
    - たまになぜか失敗するので手動で途中から動かせるようにした
    - iOSでLIVEで撮った場合＋ヤフーバックアップを使った場合はzipだけど変な拡張子なのを解凍しやすくする

--- src/yahoo_box_download.rb
+++ src/yahoo_box_download.rb
@@ -46,45 +46,69 @@ puts "appid = #{appid}"
 #ここから巡回してファイルをダウンロード	
 folderList = Array.new
 folderList.push(topuniqid)
+count=1
 #folderListが空になるまで巡回する
 while folderList.size != 0 do
 	#folderListから一つ取り出す
 	nowuniqid = folderList.pop
-	#そのフォルダ内のファイルのリストが書かれたJSONを取得する
-	urlstr = "https://box.yahoo.co.jp/api/v1/filelist/" + sid + "/" + nowuniqid + "?_=" + DateTime.now.strftime('%Q').to_s + "&"
-	urlstr << "results=#{filenum_of_page}&start=1&output=json&sort=%2Bname&filetype=both&meta=1&thumbnail=1&tree=1&sharemembercount=1&ownerinfo=1&boxcrumb="
-	urlstr << url_encode(crumb)
-	agent.get(urlstr)
-	jsonstr = JSON.parse(agent.page.body.to_s)
-	# 複数ページが存在する場合はまず全ページたどってファイル情報を入手
-	filenum = jsonstr['ObjectList']['TotalResultsAvailable'].to_s
-	unless jsonstr['ObjectList']['Object'] == nil
-		jsonstr['ObjectList']['Object'].each do |object|
-			type = object['Type'].to_s
-			name = object['Name'].to_s
-			uniqid = object['UniqId'].to_s
-			dlurl = object['Url'].to_s
-			path = "." + object['Path'].to_s #パスの先頭にドットをつけないとうまく相対パスにならない
-			#ファイルかフォルダかで処理を分岐
-			if(type == 'file') then
-				dlurl << "?appid=#{appid}&error_redirect=1&done=https%3A%2F%2Fbox.yahoo.co.jp%2Ferror%2Fdownload_error&boxcrumb="
-				dlurl << url_encode(crumb)
-				#dlurlからリダイレクトされたURLを取得 これがダウンロードリンク
-				agent.get(dlurl)
-				redirect_link = agent.page.uri.to_s
-				#ファイルを保存
-				#File.write(path, Net::HTTP.get(URI.parse(redirect_link)))
-				open(redirect_link) do |file|
-					open(path, "w+b") do |out|
-						out.write(file.read)
+	start = 1
+	filenum = 100 # 仮で入れる
+	while filenum >= start do
+		#そのフォルダ内のファイルのリストが書かれたJSONを取得する
+		urlstr = "https://box.yahoo.co.jp/api/v1/filelist/" + sid + "/" + nowuniqid + "?_=" + DateTime.now.strftime('%Q').to_s + "&"
+		urlstr << "results=#{filenum_of_page}&start=#{start}&output=json&sort=%2Bname&filetype=both&meta=1&thumbnail=1&tree=1&sharemembercount=1&ownerinfo=1&boxcrumb="
+		urlstr << url_encode(crumb)
+		agent.get(urlstr)
+		jsonstr = JSON.parse(agent.page.body.to_s)
+		# 複数ページが存在する場合はまず全ページたどってファイル情報を入手
+		filenum = jsonstr['ObjectList']['TotalResultsAvailable'].to_i
+		unless jsonstr['ObjectList']['Object'] == nil
+			jsonstr['ObjectList']['Object'].each do |object|
+				type = object['Type'].to_s
+				name = object['Name'].to_s
+				uniqid = object['UniqId'].to_s
+				dlurl = object['Url'].to_s
+				path = "." + object['Path'].to_s #パスの先頭にドットをつけないとうまく相対パスにならない
+				#ファイルかフォルダかで処理を分岐
+				if(type == 'file') then
+					# 途中で止まることがあるので復帰用
+					count = count + 1
+					puts count
+					if count < 1 then #止まった数字+1でそのファイルをスキップする
+						next
 					end
+					if FileTest.exist?(path) then # ファイルが既に存在する場合はスキップ
+						next
+					end
+					dlurl << "?appid=#{appid}&error_redirect=1&done=https%3A%2F%2Fbox.yahoo.co.jp%2Ferror%2Fdownload_error&boxcrumb="
+					dlurl << url_encode(crumb)
+					#dlurlからリダイレクトされたURLを取得 これがダウンロードリンク
+					agent.get(dlurl)
+					redirect_link = agent.page.uri.to_s
+					#ファイルを保存
+					#File.write(path, Net::HTTP.get(URI.parse(redirect_link)))
+					open(redirect_link) do |file|
+						# iOSのlive写真バックアップ用でzipファイルなので解凍を楽にする
+						# TODO: これの場合上のすでにファイルがある場合チェックが動かない
+						# TODO: livephoto (2).yjmultifile　のような場合は動かない（多分バックアップ時のミス）
+						if path.include?("livephoto.yjmultifile") then
+							open(path + ".zip", "w+b") do |out|
+								out.write(file.read)
+							end
+						else
+							open(path, "w+b") do |out|
+								out.write(file.read)
+							end
+						end
+					end
+					puts "Download #{path}"
+				elsif(type == 'dir') then
+					#folderListに追加してあとで巡回
+					folderList.push(uniqid)
+					Dir.mkdir(path) unless FileTest.exist?(path) # ディレクトリがある場合はスキップ
 				end
-				puts "Download #{path}"
-			elsif(type == 'dir') then
-				#folderListに追加してあとで巡回
-				folderList.push(uniqid)
-				Dir.mkdir(path)
 			end
 		end
+		start += filenum_of_page
 	end
 end
